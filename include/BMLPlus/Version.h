78f68b525e0c849877aaf2683480cd8c1ebca609
doyaGu <doyaGu@users.noreply.github.com>
2022-07-22 11:28:37 +0800

    Pump version

--- include/BMLPlus/Version.h
+++ include/BMLPlus/Version.h
@@ -3,7 +3,7 @@
 
 #define BML_MAJOR_VER 0
 #define BML_MINOR_VER 1
-#define BML_PATCH_VER 0
-#define BML_VERSION "0.1.0"
+#define BML_PATCH_VER 1
+#define BML_VERSION "0.1.1"
 
 #endif // BML_VERSION_H
