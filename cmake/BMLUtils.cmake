1e147eb4d343a87caca4ac82839d6c19f1cd526a
doyaGu <10153309+doyaGu@users.noreply.github.com>
2024-01-04 23:14:19 -0500

    Fix BMLUtils.cmake

--- cmake/BMLUtils.cmake
+++ cmake/BMLUtils.cmake
@@ -1,10 +1,9 @@
-# - Some useful macros and functions for creating bmods
+# - Some useful macros and functions for creating mod
 
 macro(add_bml_mod NAME)
     add_library(${NAME} SHARED)
     target_sources(${NAME} PRIVATE ${ARGN})
-    target_link_libraries(${NAME} PRIVATE
-            BMLPlus CK2 VxMath)
+    target_link_libraries(${NAME} PRIVATE BML CK2 VxMath)
     set_target_properties(${NAME} PROPERTIES
             SUFFIX ".bmodp"
             FOLDER "Mods")
