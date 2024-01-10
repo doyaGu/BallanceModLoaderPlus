80b7c4fe46e5658fd98debfed71bf6603ee3e61a
doyaGu <10153309+doyaGu@users.noreply.github.com>
2024-01-10 00:53:03 -0500

    Add HookUtils

---
+++ src/HookUtils.cpp
@@ -0,0 +1,88 @@
+#include "HookUtils.h"
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+#include <strsafe.h>
+#include <Psapi.h>
+
+namespace utils {
+    void OutputDebugA(const char *format, ...) {
+        char buf[4096] = {0};
+
+        va_list args;
+                va_start(args, format);
+        ::StringCchVPrintfA(buf, ARRAYSIZE(buf) - 1, format, args);
+                va_end(args);
+
+        buf[ARRAYSIZE(buf) - 1] = '\0';
+        ::OutputDebugStringA(buf);
+    }
+
+    void OutputDebugW(const wchar_t *format, ...) {
+        wchar_t buf[4096] = {0};
+
+        va_list args;
+                va_start(args, format);
+        ::StringCchVPrintfW(buf, ARRAYSIZE(buf) - 1, format, args);
+                va_end(args);
+
+        buf[ARRAYSIZE(buf) - 1] = L'\0';
+        ::OutputDebugStringW(buf);
+    }
+
+    void *GetSelfModuleHandle() {
+        MEMORY_BASIC_INFORMATION mbi;
+        return ((::VirtualQuery((LPVOID) &GetSelfModuleHandle, &mbi, sizeof(mbi)) != 0)
+                ? (HMODULE) mbi.AllocationBase : nullptr);
+    }
+
+    void *GetModuleBaseAddress(const char *modulePath) {
+        if (!modulePath)
+            return nullptr;
+
+        int size = ::MultiByteToWideChar(CP_UTF8, 0, modulePath, -1, nullptr, 0);
+        if (size == 0)
+            return nullptr;
+
+        auto ws = new wchar_t[size];
+        ::MultiByteToWideChar(CP_UTF8, 0, modulePath, -1, ws, size);
+
+        HMODULE hModule = ::GetModuleHandleW(ws);
+        delete[] ws;
+        if (!hModule)
+            return nullptr;
+
+        MODULEINFO moduleInfo;
+        ::GetModuleInformation(::GetCurrentProcess(), hModule, &moduleInfo, sizeof(moduleInfo));
+
+        return moduleInfo.lpBaseOfDll;
+    }
+
+    void ProtectRegion(void *region, uint32_t protection) {
+        MEMORY_BASIC_INFORMATION mbi;
+        VirtualQuery((LPCVOID) region, &mbi, sizeof(mbi));
+        VirtualProtect(mbi.BaseAddress, mbi.RegionSize, protection, &mbi.Protect);
+    }
+
+    uint32_t UnprotectRegion(void *region) {
+        MEMORY_BASIC_INFORMATION mbi;
+        VirtualQuery((LPCVOID) region, &mbi, sizeof(mbi));
+        VirtualProtect(mbi.BaseAddress, mbi.RegionSize, PAGE_EXECUTE_READWRITE, &mbi.Protect);
+        return mbi.Protect;
+    }
+
+    void *HookVirtualMethod(void *instance, void *hook, size_t offset) {
+        uintptr_t vtable = *((uintptr_t *) instance);
+        uintptr_t entry = vtable + offset * sizeof(uintptr_t);
+        uintptr_t original = *((uintptr_t *) entry);
+
+        uint32_t originalProtection = UnprotectRegion((void *) entry);
+        *((uintptr_t *) entry) = (uintptr_t) hook;
+        ProtectRegion((void *) entry, originalProtection);
+
+        return (void *) original;
+    }
+}
---
+++ src/HookUtils.h
@@ -0,0 +1,73 @@
+#ifndef BML_HOOKUTILS_H
+#define BML_HOOKUTILS_H
+
+#include <cstdint>
+#include <cassert>
+#include <cstring>
+
+namespace utils {
+    void OutputDebugA(const char *format, ...);
+    void OutputDebugW(const wchar_t *format, ...);
+
+    template<typename T>
+    void *TypeErase(T target) {
+        return *reinterpret_cast<void **>(&target);
+    }
+
+    template<typename T>
+    T ForceReinterpretCast(void *addr) {
+        return *reinterpret_cast<T *>(&addr);
+    }
+
+    template<typename T>
+    T ForceReinterpretCast(void *base, size_t offset) {
+        void *p = reinterpret_cast<char *>(base) + offset;
+        return *reinterpret_cast<T *>(&p);
+    }
+
+    void *GetSelfModuleHandle();
+
+    void *GetModuleBaseAddress(const char *modulePath);
+
+    void ProtectRegion(void *region, uint32_t protection);
+    uint32_t UnprotectRegion(void *region);
+
+    inline void **GetVTable(void *instance) {
+        if (instance) {
+            return reinterpret_cast<void **>(*reinterpret_cast<void **>(instance));
+        } else {
+            return nullptr;
+        }
+    }
+
+    template<typename T>
+    inline void LoadVTable(void *instance, T &table) {
+        if (instance) {
+            void **src = reinterpret_cast<void**>(*reinterpret_cast<void**>(instance));
+            void **dest = reinterpret_cast<void**>(&table);
+            for (size_t i = 0; i < sizeof(T) / sizeof(void *); ++i) {
+                dest[i] = src[i];
+            }
+        }
+    }
+
+    template<typename T>
+    inline void SaveVTable(void *instance, T &table) {
+        void **src = reinterpret_cast<void**>(&table);
+        void **dest = reinterpret_cast<void**>(*reinterpret_cast<void**>(instance));
+        uint32_t originalProtection = UnprotectRegion((void *) dest);
+        for (size_t i = 0; i < sizeof(T) / sizeof(void *); ++i) {
+            dest[i] = src[i];
+        }
+        ProtectRegion((void *) dest, originalProtection);
+    }
+
+    void *HookVirtualMethod(void *instance, void *hook, size_t offset);
+
+    template<typename T>
+    inline void *HookVirtualMethod(void *instance, T hook, size_t offset) {
+        return HookVirtualMethod(instance, TypeErase<T>(hook), offset);
+    }
+}
+
+#endif // BML_HOOKUTILS_H
