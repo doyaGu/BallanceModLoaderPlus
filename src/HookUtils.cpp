14c360af8abf5ccfec720d136e1387595c583fa0
doyaGu <10153309+doyaGu@users.noreply.github.com>
2024-01-14 12:30:40 -0500

    Reformat HookUtils.cpp

--- src/HookUtils.cpp
+++ src/HookUtils.cpp
@@ -3,10 +3,9 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
-
-#include <windows.h>
-#include <strsafe.h>
+#include <Windows.h>
 #include <Psapi.h>
+#include <strsafe.h>
 
 namespace utils {
     void OutputDebugA(const char *format, ...) {
