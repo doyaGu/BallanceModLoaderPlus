5e081c830fb6d2f141e862a4d52a5dc3f83e1833
doyaGu <10153309+doyaGu@users.noreply.github.com>
2024-01-21 13:48:32 -0500

    Update Config

--- src/Config.h
+++ src/Config.h
@@ -5,25 +5,27 @@
 #include <cctype>
 #include <string>
 #include <vector>
-#include <map>
 #include <sstream>
 #include <functional>
 
 #include "BML/IConfig.h"
 #include "BML/IMod.h"
+#include "Variant.h"
 
 class Config;
 
 class Property : public IProperty {
     friend class Config;
-    friend class GuiModMenu;
-    friend class GuiModCategory;
+    friend class Category;
 
 public:
     Property() = default;
     Property(Config *config, std::string category, std::string key);
 
+    const char *GetName() const { return m_Key.c_str(); }
+
     const char *GetString() override;
+    size_t GetStringSize();
     bool GetBoolean() override;
     int GetInteger() override;
     float GetFloat() override;
@@ -35,8 +37,8 @@ public:
     void SetFloat(float value) override;
     void SetKey(CKKEYBOARD value) override;
 
-    const char *GetComment();
-    void SetComment(const char *comment) override;
+    const char *GetComment() const { return m_Comment.c_str(); }
+    void SetComment(const char *comment) override { m_Comment = comment; }
 
     void SetDefaultString(const char *value) override;
     void SetDefaultBoolean(bool value) override;
@@ -45,35 +47,66 @@ public:
     void SetDefaultKey(CKKEYBOARD value) override;
 
     PropertyType GetType() override { return m_Type; }
+    size_t GetHash();
 
     void CopyValue(Property *o);
 
+    bool *GetBooleanPtr();
+    int *GetIntegerPtr();
+    float *GetFloatPtr();
+    CKKEYBOARD *GetKeyPtr();
+
+    void SetModified();
+
 private:
-    union {
-        bool m_Bool;
-        int m_Int = 0;
-        float m_Float;
-        CKKEYBOARD m_Key;
-    } m_Value;
-    std::string m_String;
-
+    Variant m_Value;
     PropertyType m_Type = INTEGER;
+    size_t m_Hash = 0;
     std::string m_Comment;
-
-    std::string m_Category, m_Key;
+    std::string m_Category;
+    std::string m_Key;
     Config *m_Config = nullptr;
+};
+
+class Category {
+    friend class Config;
+
+public:
+    Category() = default;
+    Category(Config *config, std::string name);
+    ~Category();
+
+    const char *GetName() const { return m_Name.c_str(); }
+
+    const char *GetComment() const { return m_Comment.c_str(); }
+    void SetComment(const char *comment) { m_Comment = comment; }
+
+    size_t GetPropertyCount() const { return m_Properties.size(); }
+    Property *GetProperty(size_t i);
+    Property *GetProperty(const char *key);
+
+    bool HasKey(const char *key);
+
+private:
+    std::string m_Name;
+    std::string m_Comment;
+    Config *m_Config = nullptr;
+
+    std::vector<Property *> m_Properties;
 };
 
 class Config : public IConfig {
     friend class Property;
-    friend class GuiModMenu;
-    friend class GuiModCategory;
 
 public:
     explicit Config(IMod *mod);
     ~Config() override;
 
     IMod *GetMod() { return m_Mod; }
+
+    size_t GetCategoryCount() const { return m_Categories.size(); }
+    Category *GetCategory(size_t i);
+    Category *GetCategory(const char *name);
 
     bool HasCategory(const char *category) override;
     bool HasKey(const char *category, const char *key) override;
@@ -94,19 +127,7 @@ private:
     IMod *m_Mod;
     std::string m_ModID;
 
-    struct Category {
-        std::string name;
-        std::string comment;
-        Config *config;
-
-        std::vector<Property *> props;
-
-        Property *GetProperty(const char *name);
-    };
-
-    Category &GetCategory(const char *name);
-
-    std::vector<Category> m_Data;
+    std::vector<Category *> m_Categories;
 };
 
 #endif // BML_CONFIG_H
