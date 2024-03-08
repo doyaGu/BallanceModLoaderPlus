b55aadd6eaadb18e653afb96714d6cf613c4e7e8
doyaGu <10153309+doyaGu@users.noreply.github.com>
2024-03-08 17:10:46 -0500

    Update VTables

--- src/VTables.h
+++ src/VTables.h
@@ -1,10 +1,9 @@
 #ifndef BML_VTABLES_H
 #define BML_VTABLES_H
 
-#include "Macros.h"
-
 #include "CKBaseManager.h"
-#include "CKInputManager.h"
+
+#include "Macros.h"
 
 template <class T>
 struct CP_CLASS_VTABLE_NAME(CKBaseManager) {
@@ -38,48 +37,6 @@ struct CP_CLASS_VTABLE_NAME(CKBaseManager) {
     CP_DECLARE_METHOD_PTR(T, CKERROR, OnPostSpriteRender, (CKRenderContext *dev));
     CP_DECLARE_METHOD_PTR(T, int, GetFunctionPriority, (CKMANAGER_FUNCTIONS Function));
     CP_DECLARE_METHOD_PTR(T, CKDWORD, GetValidFunctionsMask, ());
-};
-
-struct CP_CLASS_VTABLE_NAME(CKInputManager) : public CP_CLASS_VTABLE_NAME(CKBaseManager)<CKInputManager> {
-    CP_DECLARE_METHOD_PTR(CKInputManager, void, EnableKeyboardRepetition, (CKBOOL iEnable));
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKBOOL, IsKeyboardRepetitionEnabled, ());
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKBOOL, IsKeyDown, (CKDWORD iKey, CKDWORD *oStamp));
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKBOOL, IsKeyUp, (CKDWORD iKey));
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKBOOL, IsKeyToggled ,(CKDWORD iKey, CKDWORD *oStamp));
-    CP_DECLARE_METHOD_PTR(CKInputManager, void, GetKeyName, (CKDWORD iKey, CKSTRING oKeyName));
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKDWORD, GetKeyFromName, (CKSTRING iKeyName));
-    CP_DECLARE_METHOD_PTR(CKInputManager, unsigned char *, GetKeyboardState, ());
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKBOOL, IsKeyboardAttached, ());
-    CP_DECLARE_METHOD_PTR(CKInputManager, int, GetNumberOfKeyInBuffer, ());
-    CP_DECLARE_METHOD_PTR(CKInputManager, int, GetKeyFromBuffer, (int i, CKDWORD &oKey, CKDWORD *oTimeStamp));
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKBOOL, IsMouseButtonDown, (CK_MOUSEBUTTON iButton));
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKBOOL, IsMouseClicked, (CK_MOUSEBUTTON iButton));
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKBOOL, IsMouseToggled, (CK_MOUSEBUTTON iButton));
-    CP_DECLARE_METHOD_PTR(CKInputManager, void, GetMouseButtonsState, (CKBYTE oStates[4]));
-    CP_DECLARE_METHOD_PTR(CKInputManager, void, GetMousePosition, (Vx2DVector &oPosition, CKBOOL iAbsolute));
-    CP_DECLARE_METHOD_PTR(CKInputManager, void, GetMouseRelativePosition, (VxVector &oPosition));
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKBOOL, IsMouseAttached, ());
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKBOOL, IsJoystickAttached, (int iJoystick));
-    CP_DECLARE_METHOD_PTR(CKInputManager, void, GetJoystickPosition, (int iJoystick, VxVector *oPosition));
-    CP_DECLARE_METHOD_PTR(CKInputManager, void, GetJoystickRotation, (int iJoystick, VxVector *oRotation));
-    CP_DECLARE_METHOD_PTR(CKInputManager, void, GetJoystickSliders, (int iJoystick, Vx2DVector *oPosition));
-    CP_DECLARE_METHOD_PTR(CKInputManager, void, GetJoystickPointOfViewAngle, (int iJoystick, float *oAngle));
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKDWORD, GetJoystickButtonsState, (int iJoystick));
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKBOOL, IsJoystickButtonDown, (int iJoystick, int iButton));
-    CP_DECLARE_METHOD_PTR(CKInputManager, void, Pause, (CKBOOL pause));
-    CP_DECLARE_METHOD_PTR(CKInputManager, void, ShowCursor, (CKBOOL iShow));
-    CP_DECLARE_METHOD_PTR(CKInputManager, CKBOOL, GetCursorVisibility, ());
-    CP_DECLARE_METHOD_PTR(CKInputManager, VXCURSOR_POINTER, GetSystemCursor, ());
-    CP_DECLARE_METHOD_PTR(CKInputManager, void, SetSystemCursor, (VXCURSOR_POINTER cursor));
-};
-
-class CKIpionManager : public CKBaseManager {
-public:
-    virtual void Reset() = 0;
-};
-
-struct CP_CLASS_VTABLE_NAME(CKIpionManager) : public CP_CLASS_VTABLE_NAME(CKBaseManager)<CKIpionManager> {
-    CP_DECLARE_METHOD_PTR(CKIpionManager, void, Reset, ());
 };
 
 #endif // BML_VTABLES_H
