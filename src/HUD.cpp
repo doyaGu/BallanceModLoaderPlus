f63aac1fb7ad0f4b69cc902aa64691e200632fb5
doyaGu <10153309+doyaGu@users.noreply.github.com>
2024-09-15 22:59:18 -0400

    Separate the HUD from BMLMod

---
+++ src/HUD.cpp
@@ -0,0 +1,89 @@
+#include "HUD.h"
+
+#include "ModManager.h"
+
+HUD::HUD() : Bui::Window("HUD") {
+    SetVisibility(true);
+}
+
+HUD::~HUD() = default;
+
+ImGuiWindowFlags HUD::GetFlags() {
+    return ImGuiWindowFlags_NoDecoration |
+           ImGuiWindowFlags_NoBackground |
+           ImGuiWindowFlags_NoResize |
+           ImGuiWindowFlags_NoMove |
+           ImGuiWindowFlags_NoInputs |
+           ImGuiWindowFlags_NoBringToFrontOnFocus |
+           ImGuiWindowFlags_NoSavedSettings;
+}
+
+void HUD::OnBegin() {
+    ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
+    ImGui::SetNextWindowSize(ImGui::GetMainViewport()->Size);
+}
+
+void HUD::OnDraw() {
+    const ImVec2 contentSize = ImGui::GetContentRegionAvail();
+    ImDrawList *drawList = ImGui::GetWindowDrawList();
+
+    float oldScale = ImGui::GetFont()->Scale;
+    ImGui::GetFont()->Scale *= 1.2f;
+    ImGui::PushFont(ImGui::GetFont());
+
+    if (m_ShowTitle) {
+        constexpr auto TitleText = "BML Plus " BML_VERSION;
+        const auto titleSize = ImGui::CalcTextSize(TitleText);
+        drawList->AddText(ImVec2((contentSize.x - titleSize.x) / 2.0f, 0), IM_COL32_WHITE, TitleText);
+    }
+
+    if (m_ShowFPS) {
+        drawList->AddText(ImVec2(0, 0), IM_COL32_WHITE, m_FPSText);
+    }
+
+    if (BML_GetModManager()->IsCheatEnabled()) {
+        constexpr auto CheatText = "Cheat Mode Enabled";
+        const auto cheatSize = ImGui::CalcTextSize(CheatText);
+        drawList->AddText(ImVec2((contentSize.x - cheatSize.x) / 2.0f, contentSize.y * 0.85f), IM_COL32_WHITE, CheatText);
+    }
+
+    if (m_ShowSRTimer) {
+        drawList->AddText(ImVec2(contentSize.x * 0.03f, contentSize.y * 0.8f), IM_COL32_WHITE, "SR Timer");
+        auto srSize = ImGui::CalcTextSize(m_SRScore);
+        drawList->AddText(ImVec2(contentSize.x * 0.05f, contentSize.y * 0.8f + srSize.y), IM_COL32_WHITE, m_SRScore);
+    }
+
+    ImGui::GetFont()->Scale = oldScale;
+    ImGui::PopFont();
+}
+
+void HUD::OnProcess() {
+    OnProcess_Fps();
+    OnProcess_SRTimer();
+}
+
+void HUD::OnProcess_Fps() {
+    CKStats stats;
+    BML_GetCKContext()->GetProfileStats(&stats);
+    m_FPSCount += int(1000 / stats.TotalFrameTime);
+    if (++m_FPSTimer == 60) {
+        sprintf(m_FPSText, "FPS: %d", m_FPSCount / 60);
+        m_FPSTimer = 0;
+        m_FPSCount = 0;
+    }
+}
+
+void HUD::OnProcess_SRTimer() {
+    if (m_SRActivated) {
+        m_SRTimer += BML_GetCKContext()->GetTimeManager()->GetLastDeltaTime();
+        int counter = int(m_SRTimer);
+        int ms = counter % 1000;
+        counter /= 1000;
+        int s = counter % 60;
+        counter /= 60;
+        int m = counter % 60;
+        counter /= 60;
+        int h = counter % 100;
+        sprintf(m_SRScore, "%02d:%02d:%02d.%03d", h, m, s, ms);
+    }
+}
---
+++ src/HUD.h
@@ -0,0 +1,59 @@
+#ifndef BML_HUD_H
+#define BML_HUD_H
+
+#include "BML/Bui.h"
+
+class HUD : public Bui::Window {
+public:
+    HUD();
+    ~HUD() override;
+
+    ImGuiWindowFlags GetFlags() override;
+
+    void OnBegin() override;
+    void OnDraw() override;
+
+    void OnProcess();
+
+    void ShowTitle(bool show = true) {
+        m_ShowTitle = show;
+    }
+
+    void ShowFPS(bool show = true) {
+        m_ShowFPS = show;
+    }
+
+    void ActivateSRTimer(bool active = true) {
+        m_SRActivated = active;
+    }
+
+    void ShowSRTimer(bool show = true) {
+        m_ShowSRTimer = show;
+    }
+
+    void ResetSRTimer(bool show) {
+        m_SRTimer = 0.0f;
+        strcpy(m_SRScore, "00:00:00.000");
+        ShowSRTimer(show);
+    }
+
+    float GetSRScore() const {
+        return m_SRTimer;
+    }
+
+private:
+    void OnProcess_Fps();
+    void OnProcess_SRTimer();
+
+    int m_FPSCount = 0;
+    int m_FPSTimer = 0;
+    char m_FPSText[16] = {};
+    float m_SRTimer = 0.0f;
+    char m_SRScore[16] = {};
+    bool m_SRActivated = false;
+    bool m_ShowSRTimer = false;
+    bool m_ShowFPS = false;
+    bool m_ShowTitle = false;
+};
+
+#endif // BML_HUD_H
