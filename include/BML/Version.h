09188a1631d5378d9fd07056165e13acf693adcc
doyaGu <doyaGu@users.noreply.github.com>
2025-01-13 12:36:41 -0500

    Bump version

--- include/BML/Version.h
+++ include/BML/Version.h
@@ -3,7 +3,7 @@
 
 #define BML_MAJOR_VERSION 0
 #define BML_MINOR_VERSION 3
-#define BML_PATCH_VERSION 4
-#define BML_VERSION "0.3.4"
+#define BML_PATCH_VERSION 5
+#define BML_VERSION "0.3.5"
 
 #endif // BML_VERSION_H
