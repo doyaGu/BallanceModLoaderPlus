f514ee63094e358d870f33ed511c3f5403571356
doyaGu <doyaGu@users.noreply.github.com>
2025-02-09 18:55:11 -0500

    Add error codes for BML error handling

---
+++ include/BML/Errors.h
@@ -0,0 +1,11 @@
+#ifndef BML_ERRORS_H
+#define BML_ERRORS_H
+
+#define BML_OK						         (0)
+#define BML_ERROR_FAIL						 (-1)
+#define BML_ERROR_FROZEN				     (-2)
+#define BML_ERROR_NOT_FOUND			         (-3)
+#define BML_ERROR_NOT_IMPLEMENTED			 (-4)
+#define BML_ERROR_NOT_OUT_OF_MEMORY			 (-5)
+
+#endif // BML_ERRORS_H
