f55339c561bf111d8557b9e221bcd52a712abbd9
doyaGu <doyaGu@users.noreply.github.com>
2025-09-23 23:33:49 -0400

    Disable mouse cursor change in ImGui configuration

--- src/BMLMod.cpp
+++ src/BMLMod.cpp
@@ -617,6 +617,8 @@ void BMLMod::InitConfigs() {
 void BMLMod::InitGUI() {
     ImGuiIO &io = ImGui::GetIO();
 
+    io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange;
+
     const std::string path = BML_GetModContext()->GetDirectoryUtf8(BML_DIR_LOADER);
 
     m_ImGuiIniFilename = path + "\\ImGui.ini";
